2f169b051f79fa31890afb91d7a5e85169b08b76
theMackabu <theMackabu@gmail.com>
2024-09-13 00:13:53 -0700

    broke repo after rebase, fixed

---
+++ alloc/alloc.rs
@@ -0,0 +1,320 @@
+#![allow(dead_code)]
+
+use std::{
+    alloc::{self, Layout},
+    marker::PhantomData,
+    mem,
+    ops::{Deref, DerefMut},
+    ptr::{self, NonNull},
+};
+
+struct RawList<T> {
+    ptr: NonNull<T>,
+    cap: usize,
+}
+
+unsafe impl<T: Send> Send for RawList<T> {}
+unsafe impl<T: Sync> Sync for RawList<T> {}
+
+impl<T> RawList<T> {
+    fn new() -> Self {
+        let cap = if mem::size_of::<T>() == 0 { !0 } else { 0 };
+        RawList { ptr: NonNull::dangling(), cap }
+    }
+
+    fn grow(&mut self) {
+        assert!(mem::size_of::<T>() != 0, "capacity overflow");
+
+        let (new_cap, new_layout) = if self.cap == 0 {
+            (1, Layout::array::<T>(1).unwrap())
+        } else {
+            let new_cap = 2 * self.cap;
+            let new_layout = Layout::array::<T>(new_cap).unwrap();
+            (new_cap, new_layout)
+        };
+
+        assert!(new_layout.size() <= isize::MAX as usize, "Allocation too large");
+
+        let new_ptr = if self.cap == 0 {
+            unsafe { alloc::alloc(new_layout) }
+        } else {
+            let old_layout = Layout::array::<T>(self.cap).unwrap();
+            let old_ptr = self.ptr.as_ptr() as *mut u8;
+            unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
+        };
+
+        self.ptr = match NonNull::new(new_ptr as *mut T) {
+            Some(p) => p,
+            None => alloc::handle_alloc_error(new_layout),
+        };
+        self.cap = new_cap;
+    }
+}
+
+impl<T> Drop for RawList<T> {
+    fn drop(&mut self) {
+        let elem_size = mem::size_of::<T>();
+
+        if self.cap != 0 && elem_size != 0 {
+            unsafe {
+                alloc::dealloc(self.ptr.as_ptr() as *mut u8, Layout::array::<T>(self.cap).unwrap());
+            }
+        }
+    }
+}
+
+pub struct List<T> {
+    buf: RawList<T>,
+    len: usize,
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for List<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+impl<T> Clone for List<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        let mut list = List::new();
+        for elem in self.iter() {
+            list.push(elem.clone());
+        }
+        list
+    }
+}
+
+impl<T> List<T> {
+    fn ptr(&self) -> *mut T {
+        self.buf.ptr.as_ptr()
+    }
+
+    fn cap(&self) -> usize {
+        self.buf.cap
+    }
+
+    pub fn new() -> Self {
+        List { buf: RawList::new(), len: 0 }
+    }
+
+    pub fn from_slice(slice: &[T]) -> List<&T> {
+        let mut list = List::new();
+
+        slice.iter().enumerate().for_each(|(idx, elem)| list.insert(idx, elem));
+        list
+    }
+
+    pub fn push(&mut self, elem: T) {
+        if self.len == self.cap() {
+            self.buf.grow();
+        }
+
+        unsafe {
+            ptr::write(self.ptr().add(self.len), elem);
+        }
+
+        self.len += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            unsafe { Some(ptr::read(self.ptr().add(self.len))) }
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, elem: T) {
+        assert!(index <= self.len, "index out of bounds");
+        if self.len == self.cap() {
+            self.buf.grow();
+        }
+
+        unsafe {
+            ptr::copy(self.ptr().add(index), self.ptr().add(index + 1), self.len - index);
+            ptr::write(self.ptr().add(index), elem);
+        }
+
+        self.len += 1;
+    }
+
+    pub fn remove(&mut self, index: usize) -> T {
+        assert!(index < self.len, "index out of bounds");
+
+        self.len -= 1;
+
+        unsafe {
+            let result = ptr::read(self.ptr().add(index));
+            ptr::copy(self.ptr().add(index + 1), self.ptr().add(index), self.len - index);
+            result
+        }
+    }
+
+    pub fn drain(&mut self) -> Drain<T> {
+        let iter = unsafe { RawValIter::new(&self) };
+        self.len = 0;
+        Drain { iter, vec: PhantomData }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop() {}
+    }
+}
+
+impl<T> Deref for List<T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.ptr(), self.len) }
+    }
+}
+
+impl<T> DerefMut for List<T> {
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe { std::slice::from_raw_parts_mut(self.ptr(), self.len) }
+    }
+}
+
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+    fn into_iter(self) -> IntoIter<T> {
+        let (iter, buf) = unsafe { (RawValIter::new(&self), ptr::read(&self.buf)) };
+
+        mem::forget(self);
+
+        IntoIter { iter, _buf: buf }
+    }
+}
+
+struct RawValIter<T> {
+    start: *const T,
+    end: *const T,
+}
+
+impl<T> RawValIter<T> {
+    unsafe fn new(slice: &[T]) -> Self {
+        RawValIter {
+            start: slice.as_ptr(),
+            end: if mem::size_of::<T>() == 0 {
+                ((slice.as_ptr() as usize) + slice.len()) as *const _
+            } else if slice.len() == 0 {
+                slice.as_ptr()
+            } else {
+                slice.as_ptr().add(slice.len())
+            },
+        }
+    }
+}
+
+impl<T> Iterator for RawValIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                if mem::size_of::<T>() == 0 {
+                    self.start = (self.start as usize + 1) as *const _;
+                    Some(ptr::read(NonNull::<T>::dangling().as_ptr()))
+                } else {
+                    let old_ptr = self.start;
+                    self.start = self.start.offset(1);
+                    Some(ptr::read(old_ptr))
+                }
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let elem_size = mem::size_of::<T>();
+        let len = (self.end as usize - self.start as usize) / if elem_size == 0 { 1 } else { elem_size };
+        (len, Some(len))
+    }
+}
+
+impl<T> DoubleEndedIterator for RawValIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        if self.start == self.end {
+            None
+        } else {
+            unsafe {
+                if mem::size_of::<T>() == 0 {
+                    self.end = (self.end as usize - 1) as *const _;
+                    Some(ptr::read(NonNull::<T>::dangling().as_ptr()))
+                } else {
+                    self.end = self.end.offset(-1);
+                    Some(ptr::read(self.end))
+                }
+            }
+        }
+    }
+}
+
+pub struct IntoIter<T> {
+    _buf: RawList<T>,
+    iter: RawValIter<T>,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.iter.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.iter.next_back()
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
+    }
+}
+
+pub struct Drain<'a, T: 'a> {
+    vec: PhantomData<&'a mut List<T>>,
+    iter: RawValIter<T>,
+}
+
+impl<'a, T> Iterator for Drain<'a, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<T> {
+        self.iter.next()
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.iter.size_hint()
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for Drain<'a, T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.iter.next_back()
+    }
+}
+
+impl<'a, T> Drop for Drain<'a, T> {
+    fn drop(&mut self) {
+        for _ in &mut *self {}
+    }
+}
+
+#[macro_export]
+macro_rules! list {
+    () => ($crate::list::List::new());
+    ($($x:expr),* $(,)?) => ({
+        const LEN: usize = 0 $(+ { let _ = $x; 1 })*;
+        $crate::alloc::List::from_slice(&[$($x),*][..LEN])
+    });
+}
