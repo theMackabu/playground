7d5498008e15ae73e99a0e39236ad2aa58483928
theMackabu <theMackabu@gmail.com>
2024-09-17 20:45:08 -0700

    compress crate for git

--- editor/build.rs
+++ editor/build.rs
@@ -1,5 +1,9 @@
+use anyhow::Context;
 use serde::Deserialize;
+use std::fs::{self, File};
 use std::path::Path;
+use tar::Archive;
+use xz2::read::XzDecoder;
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -12,8 +16,8 @@ pub struct Language {
 }
 
 impl Language {
-    pub fn compile(&self) {
-        let path = Path::new("languages").join(&self.name).join("src");
+    pub fn compile(&self, languages_dir: &Path) {
+        let path = languages_dir.join(&self.name).join("src");
 
         let has_scanner = path.join("scanner.c").exists() || path.join("scanner.cc").exists();
         let scanner_is_cpp = path.join("scanner.cc").exists();
@@ -54,18 +58,37 @@ fn rerun_if_changed(path: impl AsRef<Path>) {
     println!("cargo:rerun-if-changed={}", path.as_ref().to_str().unwrap());
 }
 
+fn extract_languages(languages_dir: &Path) -> anyhow::Result<()> {
+    if languages_dir.exists() {
+        println!("Languages directory already exists. Skipping extraction.");
+        return Ok(());
+    }
+
+    let languages_archive = Path::new("languages.xz");
+    let file = File::open(languages_archive).context("Failed to open languages.xz")?;
+    let xz_decoder = XzDecoder::new(file);
+    let mut archive = Archive::new(xz_decoder);
+
+    archive.unpack(languages_dir).context("Failed to extract languages archive")?;
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
     let profile = std::env::var("PROFILE").unwrap();
+    let languages_dir = Path::new("languages");
 
     if profile.as_str() == "release" {
-        let config = std::fs::read_to_string("languages/index.toml")?;
+        extract_languages(languages_dir)?;
+
+        let config = fs::read_to_string(languages_dir.join("index.toml"))?;
         let config = toml::from_str::<Config>(&config)?;
-        let handles: Vec<_> = config.languages.into_iter().map(|lang| std::thread::spawn(move || lang.compile())).collect();
+        let handles: Vec<_> = config.languages.into_iter().map(|lang| std::thread::spawn(move || lang.compile(&languages_dir))).collect();
 
         for handle in handles {
             handle.join().expect("Compilation thread should not panic")
         }
     }
 
-    Ok(())
+    Ok(println!("cargo:rerun-if-changed=languages.xz"))
 }
